97db39cf5ef1133a80fda8596b4e8d8e5f992220
Alin <alinalin03030@gmail.com>
2025-05-06 20:26:19 +0000

    First Commit Lab2

--- src/Ex3/Vers.java
+++ src/Ex3/Vers.java
@@ -1,5 +1,52 @@
 package Ex3;
 
 public class Vers {
+    private String vers;
+    private String vocale = "aeiouAEIOU";
+
+    public Vers(String v){
+        this.vers = v;
+    }
+
+    public String getVers(){
+        return vers;
+    }
+
+    public int numarCuvinte(){
+        int count = 0;
+        boolean spaceing = false;
+
+        for(char c : vers.toCharArray()){
+            if(Character.isLetter(c)){
+                if(!spaceing){
+                    count++;
+                    spaceing = true;
+                }
+            }else{
+                spaceing = false;
+            }
+        }
+        return count;
+    }
+
+    public int numarVocale(){
+        int count = 0;
+
+        for(char c : vers.toCharArray()){
+            for(char v : vocale.toCharArray()){
+                if(c == v){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
 }
+
+
+//Se va defini o clasă Vers + , care are o variabilă membră privată un șir de caractere +
+//reprezentând versul și se va dezvolta câte un operator pentru fiecare cerință de mai sus (o
+//metodă care returnează numărul de cuvinte, o metodă care returnează numărul de vocale, etc).
+//Se va crea un vector de obiecte de tip Vers care va conține informația preluată din fișierul de
+//intrare.
